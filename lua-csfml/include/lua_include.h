da042bf4a21ca8fe3947227410179d225acad7f0
Shiranuit <yannick.combes@epitech.eu>
2019-03-26 17:03:50 +0100

    lua-csfml

---
+++ lua-csfml/include/lua_include.h
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2019
+** lua_include
+** File description:
+** lua_include
+*/
+
+#ifndef LUA_INCLUDE_H_
+#define LUA_INCLUDE_H_
+
+#include "lua.h"
+#include "lauxlib.h"
+#include "lualib.h"
+#include <stdlib.h>
+#include <unistd.h>
+
+#endif /* !LUA_INCLUDE_H_ */
---
+++ lua-csfml/main.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2019
+** main
+** File description:
+** main
+*/
+
+#include "lua_include.h"
+
+int main(void)
+{
+
+}
