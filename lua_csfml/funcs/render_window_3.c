efe8cae1e04fe5a85837ac937ccb648a1223d00e
cyril <cyril.begard@epitech.eu>
2019-03-27 15:29:27 +0100

    new branch

---
+++ lua_csfml/funcs/render_window_3.c
@@ -0,0 +1,30 @@
+/*
+** EPITECH PROJECT, 2019
+** render_window
+** File description:
+** render_window
+*/
+
+#include "lua_include.h"
+#include <SFML/Graphics.h>
+#include <SFML/Window.h>
+
+int win_get_size(lua_State *L)
+{
+    sfRenderWindow *window = 0;
+    sfVector2u vector;
+
+    if (lua_gettop(L) < 1) {
+        luaL_error(L, "Expected (Window)");
+        return (0);
+    }
+    if (lua_isuserdata(L, 1)) {
+        window = userdata_pointer(L, 1, sfRenderWindow);
+        vector = sfWindow_getSize(window);
+        lua_pushnumber(L, vector.x);
+        lua_pushnumber(L, vector.y);
+    } else {
+        luaL_error(L, "Expected (Window)");
+    }
+    return (2);
+}
