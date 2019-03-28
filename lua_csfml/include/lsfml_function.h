b126c3e8b377058497a723c81e31900558071300
cyril <cyril.begard@epitech.eu>
2019-03-28 17:40:10 +0100

    add function

--- lua_csfml/include/lsfml_function.h
+++ lua_csfml/include/lsfml_function.h
@@ -9,10 +9,24 @@
 #define LSFML_FUNCTION_H_
 
 #include "lua_include.h"
+#include <SFML/Graphics.h>
 
 int win_clear(lua_State *L);
 int win_cursor_visible(lua_State *L);
 int win_close(lua_State *L);
 int win_draw_circle_shape(lua_State *L);
+int win_get_size(lua_State *L);
+int sprite_set_texture(lua_State *L);
+int sprite_get_position(lua_State *L);
+int sprite_create(lua_State *L);
+int sprite_destroy(lua_State *L);
+int sprite_move(lua_State *L);
+int sprite_set_position(lua_State *L);
+int sprite_set_scale(lua_State *L);
+int sprite_get_scale(lua_State *L);
+int sprite_set_rotation(lua_State *L);
+int sprite_rotate(lua_State *L);
+int sprite_get_rotation(lua_State *L);
+int get_vector_2f(lua_State *L, sfVector2f *vector);
 
 #endif /* !LSFML_FUNCTION_H_ */
--- lua_csfml/lsfml.c
+++ lua_csfml/lsfml.c
@@ -9,13 +9,23 @@
 #include "lsfml_function.h"
 #include "lsfml.h"
 
-
-
 static const luaL_Reg lsfmllib[] = {
     {"window_clear", win_clear},
     {"window_setCursorVisible", win_cursor_visible},
     {"window_close", win_close},
     {"window_draw_circle", win_draw_circle_shape},
+    {"window_getSize", win_get_size},
+    {"sprite_setTexture", sprite_set_texture},
+    {"sprite_getPosition", sprite_get_position},
+    {"sprite_create", sprite_create},
+    {"sprite_destroy", sprite_destroy},
+    {"sprite_move", sprite_move},
+    {"sprite_setPosition", sprite_set_position},
+    {"sprite_setScale", sprite_set_scale},
+    {"sprite_getScale", sprite_get_scale},
+    {"sprite_setRotation", sprite_set_rotation},
+    {"sprite_rotate", sprite_rotate},
+    {"sprite_getRotation", sprite_get_rotation},
     {NULL, NULL},
 };
 
