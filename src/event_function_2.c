c21af841676a5a759e45808fa1700a4e44f3d67d
Shiranuit <yannick.combes@epitech.eu>
2019-04-04 17:19:56 +0200

    Fix Close, Fix Wrong Conversion Int to Float

--- src/event_function_2.c
+++ src/event_function_2.c
@@ -15,34 +15,34 @@ int event_mouse_scroll(lua_State *L, sfEvent *event)
 {
     lua_pushstring(L, "mouse_scroll");
     lua_pushnumber(L, event->mouseWheelScroll.x);
-    lua_pushnumber(L, event->mouseWheelScroll.y);
-    lua_pushnumber(L, event->mouseWheelScroll.delta);
+    lua_pushinteger(L, event->mouseWheelScroll.y);
+    lua_pushinteger(L, event->mouseWheelScroll.delta);
     return (4);
 }
 
 int event_button_pressed(lua_State *L, sfEvent *event)
 {
     lua_pushstring(L, "mouse_pressed");
-    lua_pushnumber(L, event->mouseButton.x);
-    lua_pushnumber(L, event->mouseButton.y);
-    lua_pushnumber(L, event->mouseButton.button);
+    lua_pushinteger(L, event->mouseButton.x);
+    lua_pushinteger(L, event->mouseButton.y);
+    lua_pushinteger(L, event->mouseButton.button);
     return (4);
 }
 
 int event_button_released(lua_State *L, sfEvent *event)
 {
     lua_pushstring(L, "mouse_released");
-    lua_pushnumber(L, event->mouseButton.x);
-    lua_pushnumber(L, event->mouseButton.y);
-    lua_pushnumber(L, event->mouseButton.button);
+    lua_pushinteger(L, event->mouseButton.x);
+    lua_pushinteger(L, event->mouseButton.y);
+    lua_pushinteger(L, event->mouseButton.button);
     return (4);
 }
 
 int event_mouse_move(lua_State *L, sfEvent *event)
 {
     lua_pushstring(L, "mouse_move");
-    lua_pushnumber(L, event->mouseMove.x);
-    lua_pushnumber(L, event->mouseMove.y);
+    lua_pushinteger(L, event->mouseMove.x);
+    lua_pushinteger(L, event->mouseMove.y);
     return (3);
 }
 
