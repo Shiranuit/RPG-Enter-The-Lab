caf1dab4d3ce96b1267f1da23f5797c86e06d33f
Shiranuit <yannick.combes@epitech.eu>
2019-04-09 17:04:33 +0200

    fix sprite, window, lsfml

--- externs/lua_script/main_menu.lua
+++ externs/lua_script/main_menu.lua
@@ -2,8 +2,14 @@
 -- =                MAIN MENU              =
 -- =========================================
 
-function draw()
+assets["play_texture"] = lsfml.texture.createFromFile("./assets/menu/button_play.png", {0, 0, 421, 171})
+local play_button = lsfml.sprite.create()
 
+play_button:setTexture(assets["play_texture"], false)
+play_button:setPosition({0, 0})
+
+function draw()
+    window:draw(play_button)
 end
 
 function update()
@@ -11,6 +17,6 @@ function update()
 end
 
 function event(...)
-    local event = {...}
+    local evt = {...}
     print(...)
 end
