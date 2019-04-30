0210d599bdf688705db73fd95f02e1369e6dc37d
Yoan <yoan.vessiere@epitech.eu>
2019-04-30 11:01:00 +0200

    elec

--- externs/lua_script/spells/bouleelec.lua
+++ externs/lua_script/spells/bouleelec.lua
@@ -10,6 +10,43 @@ function cooldownStartAtEnd()
     return true
 end
 
-function cast(self)
+function getCost()
+    return 15
+end
 
+function cast(self)
+    status, hor, ver = player.getStatus()
+    player.activateSpell()
+    world.spawnEntity(animationSpell["elecSpell"])
+    animationSpell["elecSpell"].restart()
+    player.removeMana(getCost())
+    local x_player, y_player = player.getPosition()
+    local x_touch, y_touch = x_player, y_player
+    local big = 30
+    local siz_up_left, siz_down_right = 0
+
+    if (hor == "right") then
+        x_touch = x_player + size
+        siz_up_left = y_player - big
+        siz_down_right = y_player + big
+        animationSpell["elecSpell"].setRotation(180)
+    end
+    if (hor == "left") then
+        x_touch = x_player - size
+        siz_up_left = y_player - big
+        siz_down_right = y_player + big
+        animationSpell["elecSpell"].setRotation(0)
+    end
+    if (ver == "down") then
+        y_touch = y_player + size
+        siz_up_left = x_player - big
+        siz_down_right = x_player + big
+        animationSpell["elecSpell"].setRotation(-90)
+    end
+    if (ver == "up") then
+        y_touch = y_player - size
+        siz_up_left = x_player - big
+        siz_down_right = x_player + big
+        animationSpell["elecSpell"].setRotation(90)
+    end
 end
