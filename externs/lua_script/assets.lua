64717a8832d0d769a32021a193b96cbbfc32db57
cyril <cyril.begard@epitech.eu>
2019-05-02 14:26:12 +0200

    maj

--- externs/lua_script/assets.lua
+++ externs/lua_script/assets.lua
@@ -38,6 +38,7 @@ assets["robot1"] = lsfml.texture.createFromFile("./assets/ennemy/robo/robot1.png
 -- =======================
 
 assets["slash"] = lsfml.texture.createFromFile("./assets/entity/slash.png", {0, 0, 67, 147})
+assets["laser"] = lsfml.texture.createFromFile("./assets/ennemy/robo/attack.png", {0, 0, 365, 33})
 
 -- =======================
 -- =    SPELL TEXTURE    =
